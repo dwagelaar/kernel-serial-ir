3283e94fb8c7ed71a5b218e6171c1b83f698df63
Dennis Wagelaar <dwagelaar@gmail.com>
2020-11-03 23:16:12 +0100

    Added serial_ir.c from kernel-5.8.17

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Fetches and builds the serial_ir kernel module
-# Author: Dennis Wagelaar
-set -euxo pipefail
-
-wget -O serial_ir.c https://raw.githubusercontent.com/torvalds/linux/master/drivers/media/rc/serial_ir.c
-make clean
-sudo make dkms
